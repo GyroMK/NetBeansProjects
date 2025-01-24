f04f343f4004e4675033872797f5ed52e40d2ce4
Gyro_MK <fri11gam@gmail.com>
2025-01-24 08:45:42 +0100

    update

--- ProyectoViajesDAMW/src/vista/Consola.java
+++ ProyectoViajesDAMW/src/vista/Consola.java
@@ -4,8 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Scanner;
+import modelo.Viajero;
+import modelo.Viajes;
+import modelo.Viajes.Acompanyante;
 
 /**
  *
@@ -33,6 +37,7 @@ public class Consola {
 
     Properties propiedades = new Properties();
     InputStream entrada = null;
+    Viajero viajeroActual = new Viajero();
 
     public void Menu() {
         Scanner teclado = new Scanner(System.in);
@@ -80,26 +85,111 @@ public class Consola {
         propiedades.load(entrada);
         Scanner teclado = new Scanner(System.in);
         int user;
+        Viajero v = new Viajero();
 
         do {
             System.out.println("1. " + propiedades.getProperty("insertar"));
+            System.out.println("2. " + propiedades.getProperty("consultar"));
+            System.out.println("3. " + propiedades.getProperty("generar"));
+            System.out.println("4. " + propiedades.getProperty("salir"));
             user = teclado.nextInt();
             switch (user) {
                 case 1:
+                    v = pideDatos();
                     break;
                 case 2:
+                    muestraDatos(v);
                     break;
                 case 3:
                     break;
                 case 4:
-                    break;
-                case 5:
+                    System.exit(0);
                     break;
             }
-            user = teclado.nextInt();
 
-        } while (user < 5 || user > 1);
+        } while (user < 4 || user > 1);
 
+    }
+
+    public Viajero pideDatos() {
+        Scanner teclado = new Scanner(System.in);
+        ArrayList<Viajes> ViajesRealizados = new ArrayList<>();
+        Viajero viajeroActual = new Viajero();
+
+        viajeroActual.setNombre(pideCadena(propiedades.getProperty("nombre")));
+        viajeroActual.setEdad(pideEntero(propiedades.getProperty("edad")));
+        viajeroActual.setNumViajes(pideEntero(propiedades.getProperty("numViajes")));
+
+        Viajes viajeActual = null;
+        for (int x = 0; x < viajeroActual.getNumViajes(); x++) {
+            viajeActual = new Viajes();    // Reservamos un nuevo espacio de memoria
+            viajeActual.setDestino(pideCadena(propiedades.getProperty("destino")));
+            viajeActual.setTiempo(pideEntero(propiedades.getProperty("tiempo")));
+            viajeActual.setAcompanyante(PideAcompanyante());
+            viajeActual.setValoracion(pideEntero(propiedades.getProperty("valoracion")));
+
+            ViajesRealizados.add(viajeActual);
+
+        }
+        viajeroActual.setArrViajes(ViajesRealizados);
+        return viajeroActual;
+    }
+
+    public void muestraDatos(Viajero viajeroActual) {
+        System.out.println("==============================");
+        System.out.println((propiedades.getProperty("nombre") + ": " + viajeroActual.getNombre()));
+        System.out.println((propiedades.getProperty("edad") + ": " + viajeroActual.getEdad()));
+        System.out.println((propiedades.getProperty("numViajes") + ": " + viajeroActual.getNumViajes()));
+        for (Viajes v : viajeroActual.getArrViajes()) {
+            System.out.println("\t" + (propiedades.getProperty("destino") + ": " + v.getDestino()));
+            System.out.println("\t" + (propiedades.getProperty("tiempo") + ": " + v.getTiempo()));
+            if (v.getAcompanyante() == Acompanyante.AMIGOS) {
+                System.out.println("\t" + (propiedades.getProperty("amigos")));
+            } else if (v.getAcompanyante() == Acompanyante.FAMILIA) {
+                System.out.println("\t" + (propiedades.getProperty("familia")));
+            } else {
+                System.out.println("\t" + propiedades.getProperty("solo"));
+            }
+            System.out.println("\t" + (propiedades.getProperty("valoracion") + ": " + v.getValoracion()));
+            System.out.println("------------------------");
+
+        }
+    }
+
+    public String pideCadena(String pregunta) {
+        Scanner teclado = new Scanner(System.in);
+        String cadenaPedida;
+        System.out.println(pregunta);
+        cadenaPedida = teclado.nextLine();
+        return cadenaPedida;
+    }
+
+    public int pideEntero(String pregunta) {
+        Scanner teclado = new Scanner(System.in);
+        int enteroPedido;
+        System.out.println(pregunta);
+        enteroPedido = teclado.nextInt();
+        return enteroPedido;
+    }
+
+    public Acompanyante PideAcompanyante() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("1." + propiedades.getProperty("familia") + "\n" + "2." + propiedades.getProperty("amigos") + "\n" + "3." + propiedades.getProperty("solo"));
+        int opcion = teclado.nextInt();
+        do {
+
+            switch (opcion) {
+                case 1:
+                    return Acompanyante.FAMILIA;
+                case 2:
+                    return Acompanyante.AMIGOS;
+                case 3:
+                    return Acompanyante.SOLO;
+                default:
+                    System.out.println("Introduze una opcion correcta");
+            }
+        } while (opcion > 3 || opcion < 1);
+        return Acompanyante.SOLO;
     }
 
 }
